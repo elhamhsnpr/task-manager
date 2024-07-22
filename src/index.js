accf653c046488bea2d95b82baad6a249b9949a7
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-22 16:31:37 +0200

    Add users update API

--- src/index.js
+++ src/index.js
@@ -53,6 +53,32 @@ app.get('/users/:id', async (req, res) => {
 
 })
 
+//Update User by ID
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => { allowedUpdates.includes(update) })
+
+    if (!isValidOperation) {
+        return res.status(400).send('Error : Invalid updates!')
+    }
+
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 //Create Task
 app.post('/tasks', async (req, res) => {
 
