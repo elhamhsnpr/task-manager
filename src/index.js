a034bfb8877b2cbcf7972419e78c56ab51d2a168
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-22 14:54:58 +0200

    refactor users API to user async/await

--- src/index.js
+++ src/index.js
@@ -10,41 +10,47 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 //Creat User
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
 //Get Users
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
 
-    User.find({}).then((users) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
 })
 
 //Get User by ID
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+
+    try {
+        const user = await User.findById(_id)
 
         if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
-    }).catch((e) => {
+
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
+
 })
 
 //Create Task
