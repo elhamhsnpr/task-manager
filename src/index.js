9f740c84897d07b21d4c7dce512d4d00e439c992
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-23 14:01:53 +0200

    add condition to check if the task with id not found

--- src/index.js
+++ src/index.js
@@ -139,6 +139,11 @@ app.patch('/tasks/:id', async (req, res) => {
 
     try {
         const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        
+        if (!task) {
+            return res.status(404).send()
+        }
+
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
