3046346f830736fc87ded70e134417d4507160cd
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-24 16:24:19 +0200

    restructure task models to use schema

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -12,5 +11,8 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
+
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
