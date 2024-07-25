1b88831f73b0f3934b5cdd63d9eacc0b108c4fb1
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-25 15:08:03 +0200

    install jwt library and add generateAuthtoken function to genearte token and save in db when user loging

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,8 +41,25 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number!')
             }
         }
-    }
+    },
+    token: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = await jwt.sign({ _id: user._id.toString() }, 'thisismynodejscourse')
+
+    user.token = user.token.concat({ token })
+    await user.save()
+
+    return token
+
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
