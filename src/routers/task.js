b0e41524e205d25440d9ddb561cecde8f24f2404
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-29 22:13:42 +0200

    refctore delete task to authenticate

--- src/routers/task.js
+++ src/routers/task.js
@@ -86,10 +86,11 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 })
 
 //Delete Task by ID
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
 
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
