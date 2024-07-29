94ab42709a573ee196a5ca1d33470d1d41120564
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-29 22:35:04 +0200

    Add middleware to delete user tasks when user is removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -101,6 +102,14 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
+
+//Delete user tasks when user is removed
+userSchema.pre('deleteOne', { document: true, query: false }, async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
 
 
 const User = mongoose.model('User', userSchema)
