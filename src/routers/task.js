a38b4f4ae5e978a68fd5d6cb945af17b8b000a0b
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-30 10:31:36 +0200

    Add filtering data to get task API

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,10 +23,15 @@ router.post('/tasks', auth, async (req, res) => {
 
 //Read Tasks
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
 
     try {
 
-        await req.user.populate('tasks')
+        await req.user.populate({ path: 'tasks', match })
         res.send(req.user.tasks)
 
     } catch (e) {
