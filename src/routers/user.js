627333b3dd0354b0cc30f418ab876b6ca5bdb6f0
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-07-31 13:21:09 +0200

    Add read user profile avatar endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -127,9 +127,34 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     req.user.avatar = req.file.buffer
     await req.user.save()
     res.send()
-    
+
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+//Delete Profile Avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+
+})
+
+//Read Profile Avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+    
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpeg')
+        res.send(user.avatar)
+
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 
