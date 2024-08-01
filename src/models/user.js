c3c729e278ffd57d0d3af50bee22344323735795
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-01 18:28:14 +0200

    install env-cmd library and set envirmont variables

--- src/models/user.js
+++ src/models/user.js
@@ -72,7 +72,7 @@ userSchema.methods.toJSON = function () {
 }
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = await jwt.sign({ _id: user._id.toString() }, 'thisismynodejscourse')
+    const token = await jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
