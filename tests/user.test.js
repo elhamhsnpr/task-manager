949ebd6dab29db2e7ae57b3143977cc6e7622607
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-05 11:22:55 +0200

    add get profile tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
 const app = require('../src/app')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Osveh',
     email: 'osveh@gmail.com',
-    password: 'test12345!'
+    password: 'test12345!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -15,23 +22,42 @@ beforeEach(async () => {
 
 
 test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
-        name: 'Elham',
-        email: 'elham@gmail.com',
-        password: 'mypass123!'
-    }).expect(201)
+    await request(app).post('/users')
+        .send({
+            name: 'Elham',
+            email: 'elham@gmail.com',
+            password: 'mypass123!'
+        })
+        .expect(201)
 })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200)
+    await request(app).post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
 })
 
 test('Should not login nonexistent uer', async () => {
-    await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: 'test12345'
-    }).expect(400)
+    await request(app).post('/users/login')
+        .send({
+            email: userOne.email,
+            password: 'test12345'
+        })
+        .expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app).get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({})
+        .expect(200)
+})
+
+test('Should not get profile for unauthorized user', async () => {
+    await request(app).get('/users/me')
+        .send({})
+        .expect(401)
 })
