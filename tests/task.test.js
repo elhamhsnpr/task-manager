81473ddec238bd8688e6cd3e4c4bbbc3dd1a1c2f
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-05 15:57:02 +0200

    add creat task test

---
+++ tests/task.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const app = require('../src/app')
+const Task = require('../src/models/task')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('Should create task for user', async () => {
+    await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'from the task test'
+        })
+        .expect(201)
+})
